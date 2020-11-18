84f0638317c3415ed629aeecfd039efd516a4f52
gsmalves <gustavoeu2017@gmail.com>
2020-11-18 09:56:40 -0300

    Algumas soluções de desafios

---
+++ SoluçãoProblemasEssenciais/quadradoCubo.js
@@ -0,0 +1,12 @@
+let line = 5;
+
+var contador = 1
+
+if((line > 1 ) && (line<1000) ){
+  
+  while(contador<=line){
+    console.log(contador,Math.pow(contador,2),Math.pow(contador,3));
+    contador++;
+  }
+  
+}
---
+++ aritmetico/somasimples.js
@@ -0,0 +1,4 @@
+var A = parseInt(5);
+var B = parseInt(6);
+let soma = (A + B);
+console.log("SOMA = " + soma);
